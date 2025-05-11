66a308bba3c137a3ebd43139b789f6191e53a2d4
SarahMumtaz357 <154543297+SarahMumtaz357@users.noreply.github.com>
2025-05-11 17:26:02 +0500

    Add files via upload

---
+++ SQLQuery sarah mumtaz.sql
@@ -0,0 +1,13 @@
+create table airbnb_listings
+(
+ID int primary key,
+city varchar (50),
+Country varchar (50),
+Number_of_rooms int,
+Year_listed int,
+)
+
+select * from airbnb_listings
+insert into airbnb_listings values (1,'Karachi','Pakistan',3,2025),(2,'Paris','France',5,2018),(3,'Tokyo','Japan',2,2017),
+(4,'New York','USA',2,2022),(5,'Shanhai','China',4,2019)
+
